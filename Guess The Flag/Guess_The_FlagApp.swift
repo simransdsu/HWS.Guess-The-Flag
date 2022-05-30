2f52693e74b690403e8eed5aced596d48a3295f7
simransdsu <simran5590@gmail.com>
2022-05-30 15:55:58 -0700

    Initial Commit

---
+++ Guess The Flag/Guess_The_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_The_FlagApp.swift
+//  Guess The Flag
+//
+//  Created by Simran Preet Singh Narang on 2022-05-30.
+//
+
+import SwiftUI
+
+@main
+struct Guess_The_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
