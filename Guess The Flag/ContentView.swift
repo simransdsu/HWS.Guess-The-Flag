9c8fb3b9c643115d93fedca05c9595a32052a29d
Simran Preet Singh <simran5590@gmail.com>
2022-05-31 07:48:09 -0700

    Building a basic UI of Guess the Flag

--- Guess The Flag/ContentView.swift
+++ Guess The Flag/ContentView.swift
@@ -8,9 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US", "Monaco"].shuffled()
+    var correctAnswer = Int.random(in: 0...2)
+
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack(spacing: 30) {
+            VStack {
+                Text("Tap the flag of")
+                Text(countries[correctAnswer])
+            }
+            
+            ForEach(0..<3) { number in
+                
+                Button {
+                    
+                } label: {
+                    Image(countries[number])
+                        .renderingMode(.original)
+                        .border(Color.black)
+                        .shadow(color: Color.black, radius: 10, x: 5, y: 5)
+                }
+
+            }
+        }
     }
 }
 
